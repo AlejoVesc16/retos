2e06aec9e01c9ba6b6a81bd936c0a4641be12b53
AlejoVesc16 <149437301+AlejoVesc16@users.noreply.github.com>
2024-03-18 12:38:41 -0500

    Add files via upload

---
+++ reto1.js
@@ -0,0 +1,15 @@
+// Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+// https://www.javascript.com
+/* 
+comentario
+asi
+*/
+
+var variable1 = 'Hola'
+const variable2 = 'Mundo'
+
+let texto = "Esto es un String"
+let numero = 123
+var nulo = null
+var indefinida = undefined
+ console.log('Hola, JavaScript')
