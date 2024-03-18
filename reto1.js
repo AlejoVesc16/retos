73b65cc76af0074a834c161a420de76cd93f4ddd
AlejoVesc16 <149437301+AlejoVesc16@users.noreply.github.com>
2024-03-18 12:51:08 -0500

    Update reto1.js

--- reto1.js
+++ reto1.js
@@ -1,15 +1,18 @@
 // Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
-// https://www.javascript.com
+// https://developer.mozilla.org/es/docs/Web/JavaScript
 /* 
 comentario
 asi
 */
 
 var variable1 = 'Hola'
-const variable2 = 'Mundo'
+const VARIABLE_2 = 'Mundo' // Las constantes siempre en mayuscula 
 
 let texto = "Esto es un String"
 let numero = 123
 var nulo = null
 var indefinida = undefined
+
  console.log('Hola, JavaScript')
+
+ console.log(texto + ' ' + numero+ ' ' + nulo + ' ' + indefinida)
